9c1393c3441f2d8d475a20d32cb94a9fdd5595ff
Florence Chilisa <fchilisa@gmail.com>
2016-01-13 11:17:54 +0200

    add models to visit schedule

--- bcvp/bcvp_subject/visit_schedule/entries.py
+++ bcvp/bcvp_subject/visit_schedule/entries.py
@@ -4,6 +4,10 @@ from edc_constants.constants import NOT_REQUIRED, REQUIRED, ADDITIONAL, NOT_ADDI
 
 subject_crf_entries = (
     CrfTuple(10L, u'bcvp_subject', u'subjectlocator', REQUIRED, NOT_ADDITIONAL),
+    CrfTuple(100L, u'bcvp_subject', u'residencymobility', REQUIRED, NOT_ADDITIONAL),
+    CrfTuple(110L, u'bcvp_subject', u'hivcareadherence', REQUIRED, NOT_ADDITIONAL),
+    CrfTuple(120L, u'bcvp_subject', u'sexualbehaviour', REQUIRED, NOT_ADDITIONAL),
+    CrfTuple(130L, u'bcvp_subject', u'recentpartner', REQUIRED, NOT_ADDITIONAL),
     CrfTuple(200L, u'bcvp_subject', u'subjectdeathreport', NOT_REQUIRED, ADDITIONAL),
     CrfTuple(210L, u'bcvp_subject', u'subjectoffstudy', NOT_REQUIRED, ADDITIONAL))
 
