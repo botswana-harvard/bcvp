8fc7460b6b3547b62c006d3f0c6d793e08b2e9da
Florence Chilisa <fchilisa@gmail.com>
2016-01-15 13:40:47 +0200

    pep8

--- bcvp/bcvp_subject/admin/subject_death_report_admin.py
+++ bcvp/bcvp_subject/admin/subject_death_report_admin.py
@@ -11,32 +11,12 @@ class SubjectDeathReportAdmin(BaseSubjectModelAdmin):
     form = SubjectDeathReportForm
     fields = (
         "report_datetime",
-        "death_date",
         "last_date_known_alive",
         "death_cause",
         "cause_other",
-        "cause",
-#         "perform_autopsy",
-#         "cause_category",
-#         "cause_category_other",
-#         "diagnosis_code",
-#         "diagnosis_code_other",
-#         "illness_duration",
-#         "medical_responsibility",
-#         "participant_hospitalized",
-#         "reason_hospitalized",
-#         "reason_hospitalized_other",
-#         "days_hospitalized",
-#         "comment"
     )
     radio_fields = {
         "death_cause": admin.VERTICAL,
-#         "perform_autopsy": admin.VERTICAL,
-#         "participant_hospitalized": admin.VERTICAL,
-#         "cause_category": admin.VERTICAL,
-#         "diagnosis_code": admin.VERTICAL,
-#         "medical_responsibility": admin.VERTICAL,
-#         "reason_hospitalized": admin.VERTICAL
-        }
+    }
 
 admin.site.register(SubjectDeathReport, SubjectDeathReportAdmin)
