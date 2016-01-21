44217ff68b6702453aa243b1ff0eb3f4094d2554
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-01-21 16:05:45 +0200

    fixing search

--- bcvp/bcvp_subject/search.py
+++ bcvp/bcvp_subject/search.py
@@ -17,5 +17,6 @@ class CallSearchByWord(BaseSearchByWord):
         qset = (
             Q(subject_identifier__icontains=self.search_value) |
             Q(first_name__icontains=self.search_value) |
-            Q(registered_subject__identity__icontains=self.search_value))
+            Q(registered_subject__identity__icontains=self.search_value) |
+            Q(registered_subject__first_name__icontains=self.search_value))
         return qset
