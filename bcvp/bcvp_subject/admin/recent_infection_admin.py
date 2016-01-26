33f1612b49967eadfd5130d64a50a3bb9b1481b8
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-01-26 10:55:25 +0200

    added identity to recent infections display list

--- bcvp/bcvp_subject/admin/recent_infection_admin.py
+++ bcvp/bcvp_subject/admin/recent_infection_admin.py
@@ -14,6 +14,7 @@ class RecentInfectionAdmin(BaseSubjectModelAdmin):
         'subject_identifier',
         'first_name',
         'initials',
+        'identity',
         'born',
         'specimen_identifier',
         'subject_cell',
