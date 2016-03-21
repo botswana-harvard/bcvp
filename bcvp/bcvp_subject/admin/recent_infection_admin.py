0b20f85e2f1f5778da36b735cb802bb23a264d66
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-03-21 15:42:36 +0200

    fixing export to csv in recent infections

--- bcvp/bcvp_subject/admin/recent_infection_admin.py
+++ bcvp/bcvp_subject/admin/recent_infection_admin.py
@@ -1,4 +1,8 @@
+from collections import OrderedDict
+
 from django.contrib import admin
+
+from edc_export.actions import export_as_csv_action
 
 from ..models import RecentInfection
 
@@ -30,5 +34,22 @@ class RecentInfectionAdmin(BaseSubjectModelAdmin):
     search_fields = (
         'subject_identifier', 'identity', 'initials',
         'specimen_identifier', 'subject_cell', 'subject_cell_alt')
+
+    actions = [
+        export_as_csv_action(
+            description="Export to CSV file",
+            fields=[],
+            delimiter=',',
+            exclude=['user_created', 'user_modified', 'hostname_created', 'hostname_modified'],
+            extra_fields=OrderedDict(
+                {'subject_identifier': 'subject_visit__appointment__registered_subject__subject_identifier',
+                 'gender': 'subject_visit__appointment__registered_subject__gender',
+                 'dob': 'subject_visit__appointment__registered_subject__dob',
+                 'screened': 'subject_visit__appointment__registered_subject__screening_datetime',
+                 'registered': 'subject_visit__appointment__registered_subject__registration_datetime',
+                 'visit_code': 'subject_visit__appointment__visit_definition__code',
+                 'visit_reason': 'subject_visit__reason',
+                 'visit_study_status': 'subject_visit__study_status'}),
+        )]
 
 admin.site.register(RecentInfection, RecentInfectionAdmin)
